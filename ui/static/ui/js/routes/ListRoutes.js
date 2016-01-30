33d00669a127ad72731e93df5bffcea3438aef2f
Brian O'Hare <bjohare@gmail.com>
2016-01-30 14:52:47 +0000

    Minor fix.

--- ui/static/ui/js/routes/ListRoutes.js
+++ ui/static/ui/js/routes/ListRoutes.js
@@ -130,7 +130,6 @@ route.list = (function() {
                 var html = '<div class="panel panel-default">' +
                     '<div id="heading-wrap" class="panel-heading"><span class="glyphicon-heading glyphicon glyphicon-list pull-left">&nbsp</span>' +
                     '<div id="heading"><h5>' + group + '</h5></div></div>' +
-                    '<div id="panel" class="panel-body"><p>Here is a list of Routes for ' + group + '</div>' +
                     '<ul id="' + groupId + '"' + 'class="list-group"></ul>' +
                     '</div>';
                 $('#routes').append(html);
