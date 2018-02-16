903ae7fbbb400a7926ed76e26cd3b1eff1902c18
= <=>
2018-02-16 15:49:48 +0000

    Fixes for HTTPS

--- ui/static/ui/js/Config.js
+++ ui/static/ui/js/Config.js
@@ -1,7 +1,7 @@
 var Config = {};
 
 Config.MAPQUEST_KEY = 'Fmjtd|luur2h082g%2Cb2%3Do5-9wbx1f';
-Config.ELEVATION_API_URL = 'http://open.mapquestapi.com/elevation/v1/profile?key=';
+Config.ELEVATION_API_URL = 'https://open.mapquestapi.com/elevation/v1/profile?key=';
 Config.TRACK_API_URL = '/comap/api/tracks';
 Config.WAYPOINT_API_URL = '/comap/api/waypoints';
 Config.WAYPOINT_MEDIA_API_URL = '/comap/api/waypointmedia'
